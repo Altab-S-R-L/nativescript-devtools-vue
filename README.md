eea0b4fd85924e02425b8f38f23774f6aa43e31c
Upd4ting <upd4ting@gmail.com>
2020-10-31 22:37:03 +0100

    feat link with socket.io instead of socketio

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# nativescript-vue-devtools
+# nativescript-vue-dev.tools
 
 A NativeScript-Vue plugin for connecting to the standalone vue-devtools.
+This version is using `nativescript-socket.io` instead of `nativescript-socketio`.
 
 [See documentation for more details](https://nativescript-vue.org/en/docs/getting-started/vue-devtools/)
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const devtools = require('@vue/devtools');
-const {isAndroid} = require('@nativescript/core')
+const devtools = require("@vue/devtools");
+const { isAndroid } = require("@nativescript/core");
 
 if (!global.performance) {
   global.performance = {};
@@ -10,20 +10,20 @@ if (!global.performance.now) {
 
   global.performance.now = function now() {
     return Date.now() - nowOffset;
-  }
+  };
 }
 
 if (!global.requestAnimationFrame) {
   global.requestAnimationFrame = function raf(cb) {
-    return setTimeout(cb, 1000 / 60)
-  }
+    return setTimeout(cb, 1000 / 60);
+  };
 }
 
 // fixes a crash when running VueDevtools
 if (!global.HTMLElement) {
-  global.HTMLElement = function() {
-    return false
-  }
+  global.HTMLElement = function () {
+    return false;
+  };
 }
 
 /**
@@ -34,87 +34,96 @@ if (!global.HTMLElement) {
  */
 function getServerIpAddress(host, port) {
   if (host) {
-    return `${host}:${port}`
+    return `${host}:${port}`;
   }
 
   if (isAndroid) {
-    const FINGERPRINT = android.os.Build.FINGERPRINT
+    const FINGERPRINT = android.os.Build.FINGERPRINT;
     if (FINGERPRINT.includes("vbox")) {
       // running on genymotion
-      return `10.0.3.2:${port}`
+      return `10.0.3.2:${port}`;
     } else if (FINGERPRINT.includes("generic")) {
       // running on android emulator
-      return `10.0.2.2:${port}`
+      return `10.0.2.2:${port}`;
     }
   }
 
   // ios simulator uses localhost
-  return `127.0.0.1:${port}`
+  return `127.0.0.1:${port}`;
 }
 
 // Wrap the toast message in a try, devtool still work without toaster
 const showToast = (message) => {
   try {
-    const Toasty = require('nativescript-toasty-ns-7').Toasty;
+    const Toasty = require("nativescript-toasty").Toasty;
     new Toasty({
       text: message,
-      backgroundColor: '#53ba82',
-      yAxisOffset: 50
+      backgroundColor: "#53ba82",
+      yAxisOffset: 50,
     }).show();
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-module.exports = function install(Vue, {debug = false, host = null, port = 8098} = {}) {
+module.exports = function install(
+  Vue,
+  { debug = false, host = null, port = 8098 } = {}
+) {
   // ensure all dependencies are available and bail out if anything is missing
   try {
-    require.resolve('nativescript-socketio')
-    require.resolve('nativescript-toasty-ns-7')
-    require.resolve('@vue/devtools')
+    require.resolve("nativescript-socket.io");
+    require.resolve("nativescript-toasty");
+    require.resolve("@vue/devtools");
   } catch (e) {
-    console.log('[nativescript-vue-devtools] skipping install due to missing dependencies.\n\n', e, '\n\n')
-    return
+    console.log(
+      "[nativescript-vue-devtools] skipping install due to missing dependencies.\n\n",
+      e,
+      "\n\n"
+    );
+    return;
   }
 
-  const startApp = Vue.prototype.$start
+  const startApp = Vue.prototype.$start;
 
   Vue.prototype.$start = function () {
     const setupDevtools = () => {
-      devtools.connect('ws://localhost', port, {
+      devtools.connect("ws://localhost", port, {
         app: this,
         showToast,
         io() {
           try {
-            const SocketIO = require('nativescript-socketio').SocketIO
-            const address = `http://${getServerIpAddress(host, port)}`
-            let socketIO = new SocketIO(address, {debug: debug})
-            socketIO.connect()
-
-            return socketIO
+            const SocketIO = require("nativescript-socket.io");
+            if (debug) {
+              SocketIO.enableDebug();
+            }
+            const address = `http://${getServerIpAddress(host, port)}`;
+            let socketIO = SocketIO.connect(address);
+            socketIO.connect();
+            return socketIO;
           } catch (error) {
-            console.log(error)
+            console.log(error);
             // prevent crashing by returning a noop stub
             return {
               on() {},
-              emit() {}
-            }
+              emit() {},
+            };
           }
-        }
-      })
+        },
+      });
 
       devtools.init(Vue);
-    }
+    };
 
     if (isAndroid) {
-      setupDevtools()
+      setupDevtools();
     } else {
       // on ios we need to delay the call because the socket connection is not established
       // if called too early in the application startup process
       // we might need to add a delay to the setTimeout in the future
-      setTimeout(setupDevtools)
+      setTimeout(setupDevtools);
     }
 
-    return startApp.call(this)
-  }
-}
+    return startApp.call(this);
+  };
+};
